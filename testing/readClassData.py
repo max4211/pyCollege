9f9730cc10eadc06dc579a8f7d674d9fba5cfd2c
max4211 <44125945+max4211@users.noreply.github.com>
2019-08-24 19:17:35 -0400

    Read data first iter complete

--- testing/readClassData.py
+++ testing/readClassData.py
@@ -59,25 +59,92 @@ def class_info(sheet, row):
     # Extract info to keep
     for i in range(sheet.ncols):
         header.append(sheet.cell_value(0, i))
-        data.append(sheet.cell_value(row, i))
-    
-    # Print header info
-    for i in range(len(header)):
-        print(header[i])
-
-    # Print data info
-    for i in range(len(data)):
-        print(data[i])
+        data.append(sheet.cell_value(1, i))
 
     message = (f"Header Length: {len(header)}, Data Length: {len(data)}")
     log_and_print(message)
 
-    return data, header
+    return header, data
+
+def conv_excel_time(time):
+    # TODO, use library to convert this
+    new_time = time
+    return new_time
     
 class Course(object):
+    # Declare course variables
+    def __init__(self, days, start_times, end_times, class_type):
+        if days is None:
+            days = []
+        if start_times is None:
+            start_times = []
+        if end_times is None:
+            end_times = []
+        if class_type is None:
+            class_type = []
 
-    pass
+def class_dict(header, data, convert_time):
+    # Initialize empty data stores to hold class information
+    # TODO Should these be sets or lists?
+    days, start_times, end_times, class_type = [], [], [], []
+    UCD_num, course_name, Duke_num, grading = "", "", "", ""
 
+    # Parse sheet data and populate elements
+    for i in range(len(header)):
+        i_type = header[i]
+        cell_info = data[i]
+        # Skip over empty information (not all classes have same number of meeting times)
+        if cell_info == "":
+            pass
+        else:
+            if (i_type == "Day"):
+                days.append(cell_info)
+            elif (i_type == "Type"):
+                class_type.append(cell_info)
+
+            # TODO Time converter for debug 
+            # NOTE Excel form likely very useful for computation
+            elif (i_type == "Start Time"): 
+                if convert_time:
+                    time_info = conv_excel_time(cell_info)
+                else:
+                    time_info = cell_info
+                start_times.append(time_info)
+            elif (i_type == "End Time"):
+                if convert_time:
+                    time_info = conv_excel_time(cell_info)
+                else:
+                    time_info = cell_info
+                end_times.append(time_info)
+            # TODO Automatically append header as key - would need dynamic dict for this
+            # Otherwise, information is administrative, want the key to be header info
+            elif (i_type == "UCD Number"):
+                UCD_num = cell_info
+            elif (i_type == "Course Name"):
+                course_name = cell_info
+            elif (i_type == "Duke Credit"):
+                Duke_num = cell_info
+            elif (i_type == "Grading"):
+                grading == cell_info
+
+    # Populate dictionary
+    my_class = {
+        "UCD_num" : UCD_num,
+        "course_name" : course_name,
+        "Duke_num" : Duke_num,
+        "days" : days,
+        "start_times" : start_times,
+        "end_times" : end_times,
+        "class_type" : class_type
+    }
+
+    # Print to verify information
+    for i in my_class.keys():
+        message = (f"Key: {i}, Value: {my_class[i]}")
+        log_and_print(message)
+
+    # Return class dictionary
+    return my_class
 
 root = os.path.join("C:\\", "Users", "Max Smith", "Desktop", "GitHub", "pyCollege", "testing")
 check_directory(root)
@@ -93,7 +160,9 @@ w = xlrd.open_workbook(inpath)
 sheet = w.sheet_by_name("pyCollege_xl")
 
 # Call actions on the sheet
-extract_col(sheet)
-row, col = sheet_stats(sheet)
-data, header = class_info(sheet, 1)
+# extract_col(sheet)
+# row, col = sheet_stats(sheet)
+header, data = class_info(sheet, 1)
+my_class = class_dict(header, data, convert_time=False)
 
+# TODO Create git ignore to ignore the xlsx file
